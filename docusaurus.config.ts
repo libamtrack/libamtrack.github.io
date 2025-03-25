be0a6fa920724291de68fd739bf6982e471ceeca
Leszek Grzanka <leszek.grzanka@gmail.com>
2025-03-25 18:16:43 +0100

    Add GitHub Actions workflow for deploying to GitHub Pages and update README with correct link (#2)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -111,7 +111,7 @@ const config: Config = {
             // },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/libamtrack',
             },
           ],
         },
